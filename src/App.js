3f576999b1f34aa1f55a788f5a70b4ff68ca5995
smil98 <seungmilee98@gmail.com>
2023-10-31 11:55:23 +0900

    Practice Components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@
 function App() {
   return (
     <div>
-
+        <MyComponent1 />
+        <MyComponent2 />
     </div>
   );
+}
+
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    )
+}
+function MyComponent2() {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequuntur dignissimos, ea eveniet
+                exercitationem explicabo perferendis possimus provident quia velit! Amet aut cum eaque earum natus quis
+                repellat ullam voluptatem!</p>
+            <ul>
+                <li>Lorem</li>
+                <li>ipsum</li>
+                <li>dolor</li>
+            </ul>
+        </>
+    )
 }
 
 export default App;
