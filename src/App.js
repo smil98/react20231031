220e3143278a712b17771e8bd5e0caa0cd280eca
smil98 <seungmilee98@gmail.com>
2023-10-31 15:21:26 +0900

    Using Logics

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 function App() {
-  //must include end tag
-  //if empty content must end right at the start ex. <br />
-  return (
-    <div>
-      <h1>My First React</h1>
-      <MyComp />
-      <MyComp2 />
-    </div>
-  );
-}
+  const number = Math.ceil(Math.random() * 100);
+  const isLarge = number > 50;
+  const isSmall = number <= 50;
 
-//syntax: components should always start with Uppercase
-//must have return statement, mostly jsx code
-function MyComp() {
-  //always should have one root tag
   return (
     <>
-      <h2>Component Example</h2>
-      <h3>Must have one root tag</h3>
-      {/*this is how th add comments*/}
+      <h1>Random Number : {number}</h1>
+      <h1>{number > 50 ? "Big" : "Small"} Number</h1>
+      <div style={{ color: "red" }}>
+        {number > 50 ? <BigImg /> : <SmallImg />}
+      </div>
+      <div>{isLarge && <h2>Its a Big Number</h2>}</div>
+      <div>{isLarge || <h2>Its a Small Number</h2>}</div>
     </>
   );
 }
 
-function MyComp2() {
-  let name = "son";
-  let number = 33;
+function BigImg() {
+  return <h1>Big Image</h1>;
+}
 
-  return (
-    <>
-      <h1>hello react</h1>
-      <h1>hello {name}</h1>
-      <h2>age {number}</h2>
-      <h3>{number * 2} double years</h3>
-      <h3>{name + " heungmin"}</h3>
-      {/* comments formed like this */}
-    </>
-  );
+function SmallImg() {
+  return <h1>Small Image</h1>;
 }
 
 export default App;
