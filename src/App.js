40586b380a6ab88aaaa0587991d69c5afddf2640
smil98 <seungmilee98@gmail.com>
2023-10-31 15:51:13 +0900

    Property's value can be javascript code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 function App() {
+  const name = "heungmin";
   return (
-    // since jsx is converted to js, cannot use jsx keywords
     <>
-      <div className="header">Cannot write div class=""</div>
-      <div className="error">Use className instead</div>
-      <div>
-        <label htmlFor="nameInput">Name</label>
-        {/*for property is used as htmlFor*/}
-        <input type="text" id="nameInput" />
-      </div>
-      <div>
-        <label htmlFor="nameInput">Age</label>
-        <input type="text" id="ageInput" />
-      </div>
+      <h1>name: {name}</h1>
+      <h1>name: {"kangin"}</h1>
+      <h1>name: heechan</h1>
+
+      <div className={"error"}>Lorem ipsum dolor.</div>
+      <div className="error">Lorem ipsum dolor.</div>
     </>
   );
 }
