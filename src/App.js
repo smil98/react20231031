89d7372191969d224a869c80caa5ed2b1f3cb93b
smil98 <seungmilee98@gmail.com>
2023-10-31 14:55:21 +0900

    write js code in jsx, using const

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-
 function App() {
   return (
     <div>
-        <h1>My First React</h1>
-        <MyComp />
+      <h1>My First React</h1>
+      <MyComp />
     </div>
   );
 }
 
-//syntax: components should always start with Uppercase
-//must have return statement, mostly jsx code
 function MyComp() {
-    //always should have one root tag
-    return (
-        <>
-            <h2>Component Example</h2>
-            <h3>Must have one root tag</h3>
-            {/*this is how th add comments*/}
-        </>
-    )
+  const address = "seoul";
+  const country = "korea";
+  const phone = "0109999999";
+  const age = 44;
+  const randomNumber = Math.random();
+
+  return (
+    <>
+      <h1>age : {age}</h1>
+      <h1>address : {address}</h1>
+      <h1>country : {country}</h1>
+      <h1>phone number : {phone}</h1>
+      <h1>Random Number : {randomNumber}</h1>
+      <h1>Random Number(1~10) : {Math.ceil(randomNumber * 10)}</h1>
+    </>
+  );
 }
 
 export default App;
