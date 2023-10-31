9138a39ff85b3dc873ddeff7aa9b325d8d87bf6a
smil98 <seungmilee98@gmail.com>
2023-10-31 16:33:22 +0900

    Giving Styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
 function App() {
+  const myStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+  };
+
   return (
-    <div>
-        <h1>My First React</h1>
-        <MyComp />
+    <div style={myStyle}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
     </div>
   );
 }
 
-//syntax: components should always start with Uppercase
-//must have return statement, mostly jsx code
 function MyComp() {
-    //always should have one root tag
-    return (
-        <>
-            <h2>Component Example</h2>
-            <h3>Must have one root tag</h3>
-            {/*this is how th add comments*/}
-        </>
-    )
+  const name = "heungMin";
+  return <h1>{name}</h1>;
 }
 
 export default App;
