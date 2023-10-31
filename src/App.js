d8dbbd0186a463be8b16e0a7f8743af935294978
smil98 <seungmilee98@gmail.com>
2023-10-31 14:41:59 +0900

    write js code in jsx, surround with {}

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1>My First React</h1>
       <MyComp />
+      <MyComp2 />
     </div>
   );
 }
@@ -18,6 +19,22 @@ function MyComp() {
       <h2>Component Example</h2>
       <h3>Must have one root tag</h3>
       {/*this is how th add comments*/}
+    </>
+  );
+}
+
+function MyComp2() {
+  let name = "son";
+  let number = 33;
+
+  return (
+    <>
+      <h1>hello react</h1>
+      <h1>hello {name}</h1>
+      <h2>age {number}</h2>
+      <h3>{number * 2} double years</h3>
+      <h3>{name + " heungmin"}</h3>
+      {/* comments formed like this */}
     </>
   );
 }
