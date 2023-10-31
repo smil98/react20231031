8a72e14712e26f3c494b163d98e473a76d723a5f
smil98 <seungmilee98@gmail.com>
2023-10-31 15:34:15 +0900

    Using Logics (Practice)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-
 function App() {
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
   return (
     <div>
-        <h1>My First React</h1>
-        <MyComp />
+      <h1>Number 1: {num1}</h1>
+      <h1>Number 2: {num2}</h1>
+      <h2>{num1 == num2 ? "Congrats!" : "Re-Throw the Dice"}</h2>
     </div>
   );
-}
-
-//syntax: components should always start with Uppercase
-//must have return statement, mostly jsx code
-function MyComp() {
-    //always should have one root tag
-    return (
-        <>
-            <h2>Component Example</h2>
-            <h3>Must have one root tag</h3>
-            {/*this is how th add comments*/}
-        </>
-    )
 }
 
 export default App;
