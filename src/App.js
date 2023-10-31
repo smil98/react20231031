b6b075f28f1bd308d9882a76d42983dc1d9774e9
smil98 <seungmilee98@gmail.com>
2023-10-31 14:08:40 +0900

    jsx code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 function App() {
+  //must include end tag
+  //if empty content must end right at the start ex. <br />
   return (
     <div>
-        <h1>My First React</h1>
-        <MyComp />
+      <h1>My First React</h1>
+      <MyComp />
     </div>
   );
 }
@@ -11,14 +12,14 @@ function App() {
 //syntax: components should always start with Uppercase
 //must have return statement, mostly jsx code
 function MyComp() {
-    //always should have one root tag
-    return (
-        <>
-            <h2>Component Example</h2>
-            <h3>Must have one root tag</h3>
-            {/*this is how th add comments*/}
-        </>
-    )
+  //always should have one root tag
+  return (
+    <>
+      <h2>Component Example</h2>
+      <h3>Must have one root tag</h3>
+      {/*this is how th add comments*/}
+    </>
+  );
 }
 
 export default App;
