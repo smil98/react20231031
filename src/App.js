bfe6bd75f41b79d1e761fa082c71435883e1a50f
smil98 <seungmilee98@gmail.com>
2023-10-31 11:40:02 +0900

    Component, Comments, jsx

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@
 function App() {
   return (
     <div>
-
+        <h1>My First React</h1>
+        <MyComp />
     </div>
   );
+}
+
+//syntax: components should always start with Uppercase
+//must have return statement, mostly jsx code
+function MyComp() {
+    //always should have one root tag
+    return (
+        <>
+            <h2>Component Example</h2>
+            <h3>Must have one root tag</h3>
+            {/*this is how th add comments*/}
+        </>
+    )
 }
 
 export default App;
