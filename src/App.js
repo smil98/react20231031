86b5cc1f60d72d8c69a5e9ec7bac7366c16cec63
smil98 <seungmilee98@gmail.com>
2023-11-01 11:36:05 +0900

    Destructuring props practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-
 function App() {
   return (
     <div>
-        <h1>My First React</h1>
-        <MyComp />
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hello react" />
+      <MyComp m="30px" p="10px" color="gold" bg="black" />
+      <MyComp m="30px" bg="black" text="hello react" />
+      <MyComp p="10px" color="gold" text="hello react" />
     </div>
   );
 }
 
-//syntax: components should always start with Uppercase
-//must have return statement, mostly jsx code
-function MyComp() {
-    //always should have one root tag
-    return (
-        <>
-            <h2>Component Example</h2>
-            <h3>Must have one root tag</h3>
-            {/*this is how th add comments*/}
-        </>
-    )
+function MyComp({
+  m = "30px",
+  p = "10px",
+  color = "pink",
+  bg = "beige",
+  text = "default text",
+}) {
+  return (
+    <>
+      <p style={{ margin: m, padding: p, color: color, backgroundColor: bg }}>
+        {text}
+      </p>
+    </>
+  );
 }
 
 export default App;
