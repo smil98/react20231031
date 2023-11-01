b0c8fb8683ea3fea9ccad81f0441d70256d4f8df
smil98 <seungmilee98@gmail.com>
2023-11-01 14:27:40 +0900

    named export, default export practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-
+import MyBox, { person, team } from "./component/MyContainer";
 function App() {
   return (
     <div>
-        <h1>My First React</h1>
-        <MyComp />
+      <MyBox />
+      <h1>{person.name}</h1>
+      <h1>{person.address}</h1>
+      <h1>{team.title}</h1>
+      <h1>{team.score}</h1>
     </div>
   );
-}
-
-//syntax: components should always start with Uppercase
-//must have return statement, mostly jsx code
-function MyComp() {
-    //always should have one root tag
-    return (
-        <>
-            <h2>Component Example</h2>
-            <h3>Must have one root tag</h3>
-            {/*this is how th add comments*/}
-        </>
-    )
 }
 
 export default App;
---
+++ src/component/MyContainer.js
@@ -0,0 +1,13 @@
+function MyBox() {
+  return (
+    <div>
+      <h1>Practice</h1>
+    </div>
+  );
+}
+
+const person = { name: "heungmin", address: "seoul" };
+const team = { title: "blah", score: 30 };
+
+export { person, team };
+export default MyBox;
