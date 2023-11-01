883473fe3d5f92246e7dca434770a1eae834029e
smil98 <seungmilee98@gmail.com>
2023-11-01 11:47:49 +0900

    dividing components to seperate files and import/exporting them

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+import { MyElem } from "./component/MyElem";
+import { MyComp } from "./component/MyComp";
+
 function App() {
   return (
     <>
-      <MyComp name="heungmin" address="sincheon" city="seoul" age={33} />
-      <MyComp address="gangnam" city="busan" age={22} />
-      <MyComp address="london" city="london" />
-      <MyComp name="heechan" address="gangnam" city="paris" age={66} />
+      <MyComp />
+      <MyElem />
     </>
   );
 }
 
-function MyComp({ name = "Anon", address, city, age = 100 }) {
-  // let {value, address} = props;
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        lives in {address}, currently working in {city} and is currently{" "}
-        {age + " "} years old.
-      </p>
-    </div>
-  );
-}
 export default App;
---
+++ src/component/MyComp.js
@@ -0,0 +1,3 @@
+export function MyComp() {
+  return null;
+}
