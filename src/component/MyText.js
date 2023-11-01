371bf98560f4dbc745a541c368fedeb4742c5bc9
smil98 <seungmilee98@gmail.com>
2023-11-01 17:14:43 +0900

    Applying styles: if there are multiple classes to assign (practice)

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -2,7 +2,12 @@ import myStyle from "../style/mystyle.module.css";
 export default function MyText() {
   return (
     <div>
-      <h1 className={myStyle.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${myStyle.error} ${myStyle.error} ${myStyle.coffee}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <h1 className={[myStyle.error, myStyle.error, myStyle.coffee].join(" ")}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }
