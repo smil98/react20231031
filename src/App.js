cc5fd0efdf5d132c97dcc76ff04f06f80ed102a7
smil98 <seungmilee98@gmail.com>
2023-11-01 15:51:09 +0900

    Applying styles: if class with same class name is assigned

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@
 //css module
 //chakraui css library
 import MyBox from "./component/MyBox";
+import MyContainer from "./component/MyContainer";
 
 function App() {
   return (
     <>
       <MyBox />
       <MyBox />
+      <MyContainer />
+      <MyContainer />
     </>
   );
 }
---
+++ src/component/MyContainer.js
@@ -0,0 +1,8 @@
+import "../css/style2.css";
+export default function MyContainer() {
+  return (
+    <div>
+      <h1 className="boxClass">Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
