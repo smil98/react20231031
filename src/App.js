aa1e8a90bfbbbb7987b237b0515a14e564a2309a
smil98 <seungmilee98@gmail.com>
2023-11-02 09:34:59 +0900

    Unnecessary Codes Deleted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-//how to apply style
-//css file
-//css module
-//chakraui css library
-import MyBox from "./component/MyBox";
-import MyContainer from "./component/MyContainer";
-import { MyComp } from "./component/MyComp";
-
 function App() {
-  return (
-    <>
-      <MyBox />
-      <MyComp />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
