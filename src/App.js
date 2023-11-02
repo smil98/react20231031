4442e8f2a5ff92c3a1e49b2a4385926ecfe8db46
smil98 <seungmilee98@gmail.com>
2023-11-02 14:12:05 +0900

    Modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import { Box, Center, Circle, Square } from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Center bg={"blue"} h={"200px"}>
-        <Square bg={"white"} w={"100px"} h={"100px"}>
-          Square
-        </Square>
-      </Center>
-      <Center bg={"red.200"} h={"200px"}>
-        <Circle bg={"white"} w={"100px"} h={"100px"}>
-          <AddIcon />
-        </Circle>
-      </Center>
+      <Button onClick={onOpen}>Open Modal</Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Notice</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+            blanditiis cupiditate ea esse, eveniet facilis, fugit illo iste
+            labore laboriosam laborum molestiae perferendis qui repudiandae
+            sequi, suscipit tenetur ullam voluptates!
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
