4088994d1fc72bb39c7298147ab9418dc526099e
smil98 <seungmilee98@gmail.com>
2023-11-02 10:26:49 +0900

    Emptied App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <>
-      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
-      <Box bgColor="yellow.100">Lorem ipsum dolor.</Box>
-      <Box bgColor="blue">Illo, itaque, possimus.</Box>
-      <Box bgColor="gray.700">Adipisci, dolores, rerum!</Box>
-      <Box bgColor="beige" color="red">
-        Adipisci esse, necessitatibus.
-      </Box>
-      <Box bgColor="red" color="#fff">
-        Hic, necessitatibus sint.
-      </Box>
-      <Box bgColor="blue.700" color="whiteAlpha.900">
-        Ab corporis, laboriosam.
-      </Box>
-      <Box bgColor="orange" color="black.900">
-        Animi, aperiam, sed!
-      </Box>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
