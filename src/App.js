5e2845ffc9e8390d7938f366a1bef6ee985798f5
smil98 <seungmilee98@gmail.com>
2023-11-02 11:32:42 +0900

    SimpleGrid, responsive web application

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Flex
-        w={"100%"}
-        h={"100px"}
-        bg={"gray.300"}
-        gap={3}
-        align={"center"}
-        justify={"space-around"}
-      >
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"blue"} color={"white"}>
-          Deleniti!
-        </Box>
-        <Box bg={"white"}>Harum!</Box>
-        <Box bg={"red"}>Pariatur!</Box>
-        <Box bg={"orange"}>Saepe.</Box>
-        <Box bg={"green"} color={"white"}>
-          Laborum!
-        </Box>
-      </Flex>
+      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
+        {/* When sm, 2 col, when md, 3 col, thus responsive */}
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Adipisci.</Box>
+        <Box bg="blue.300">Consequuntur?</Box>
+        <Box bg="blue.300">Nostrum!</Box>
+        <Box bg="blue.300">Sapiente!</Box>
+        <Box bg="blue.300">Adipisci.</Box>
+        <Box bg="blue.300">Nemo.</Box>
+        <Box bg="blue.300">Alias.</Box>
+        <Box bg="blue.300">Quos?</Box>
+        <Box bg="blue.300">Culpa?</Box>
+        <Box bg="blue.300">Qui!</Box>
+        <Box bg="blue.300">Vero.</Box>
+        <Box bg="blue.300">Ut!</Box>
+        <Box bg="blue.300">Rerum.</Box>
+        <Box bg="blue.300">Ratione.</Box>
+        <Box bg="blue.300">Asperiores!</Box>
+        <Box bg="blue.300">Quidem.</Box>
+        <Box bg="blue.300">Reiciendis?</Box>
+        <Box bg="blue.300">Dolore!</Box>
+        <Box bg="blue.300">Labore?</Box>
+      </SimpleGrid>
     </>
   );
 }
