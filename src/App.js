828d4addefab42a3bb7abd97397a413d70b694f7
smil98 <seungmilee98@gmail.com>
2023-11-02 15:41:27 +0900

    Image

--- src/App.js
+++ src/App.js
@@ -1,51 +1,42 @@
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Button,
-  Input,
-  Center,
-  Box,
-  RadioGroup,
-  Radio,
-  Flex,
-  Checkbox,
-  CheckboxGroup,
-  Select,
-  Textarea,
-  Switch,
-} from "@chakra-ui/react";
+import { Box, Circle, Image, Stack } from "@chakra-ui/react";
 import { AddIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <div>
-      <Box w={"480px"} m={10}>
-        <FormControl>
-          <Select placeholder="---Select Options---" multiple h={"100px"}>
-            <option value="value1">Lorem.</option>
-            <option value="value2">ipsum</option>
-            <option value="value3">dolor</option>
-            <option value="value4">sit</option>
-            <option value="value5">amet</option>
-          </Select>
-        </FormControl>
+      <Box boxSize="sm">
+        <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
       </Box>
-      <Box w={"480px"} m={10}>
-        <FormControl>
-          <FormLabel>Content</FormLabel>
-          <Textarea placeholder="Write Content" size="sm"></Textarea>
-        </FormControl>
-      </Box>
-      <Box w={"480px"} m={10}>
-        <FormControl display="flex" alignitems="center">
-          <FormLabel htmlFor="email-alerts" mb={0}>
-            Enable Email Alerts?
-          </FormLabel>
-          <Switch id="email-alerts" size="lg" />
-        </FormControl>
-      </Box>
+      <Stack direction="column">
+        <Image
+          boxSize="100px"
+          objectFit="cover"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          objectFit="cover"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="200px"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+        <Image
+          borderRadius="full"
+          boxSize="250px"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="300px"
+          src="abcde.jpg"
+          fallbackSrc="https://via.placeholder.com/150"
+        />
+      </Stack>
     </div>
   );
 }
