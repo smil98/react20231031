c65d87772249cd6c72d0f2c466f5471b1be5c069
smil98 <seungmilee98@gmail.com>
2023-11-02 15:25:47 +0900

    Select Options, TextArea, Switch

--- src/App.js
+++ src/App.js
@@ -12,51 +12,40 @@ import {
   Flex,
   Checkbox,
   CheckboxGroup,
+  Select,
+  Textarea,
+  Switch,
 } from "@chakra-ui/react";
 import { AddIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <div>
-      <Center p={5}>
-        <Box w={"480px"}>
-          <FormControl border={"1px solid gray"} borderRadius={5} p={5}>
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-            <FormHelperText>Write your Email</FormHelperText>
-          </FormControl>
-        </Box>
-        <br />
-      </Center>
-      <Center>
-        <Box w={"480px"} border={"1px solid blue"}>
-          <FormControl p={5}>
-            <FormLabel>Select One Option Only</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Maiores.</Radio>
-                <Radio value="value3">Corporis?</Radio>
-                <Radio value="value4">Enim.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
-        <Box w={"480px"} border={"1px dotted black"}>
-          <FormControl p={5}>
-            <FormLabel>Select Options</FormLabel>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox>Lorem.</Checkbox>
-                <Checkbox>Eaque!</Checkbox>
-                <Checkbox>Sapiente?</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w={"480px"} m={10}>
+        <FormControl>
+          <Select placeholder="---Select Options---" multiple h={"100px"}>
+            <option value="value1">Lorem.</option>
+            <option value="value2">ipsum</option>
+            <option value="value3">dolor</option>
+            <option value="value4">sit</option>
+            <option value="value5">amet</option>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box w={"480px"} m={10}>
+        <FormControl>
+          <FormLabel>Content</FormLabel>
+          <Textarea placeholder="Write Content" size="sm"></Textarea>
+        </FormControl>
+      </Box>
+      <Box w={"480px"} m={10}>
+        <FormControl display="flex" alignitems="center">
+          <FormLabel htmlFor="email-alerts" mb={0}>
+            Enable Email Alerts?
+          </FormLabel>
+          <Switch id="email-alerts" size="lg" />
+        </FormControl>
+      </Box>
     </div>
   );
 }
