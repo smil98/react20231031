7e761547db95c90a243070e6d84173161eb25de1
smil98 <seungmilee98@gmail.com>
2023-11-02 10:20:18 +0900

    Apply Color, Background Color style using Chakra UI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
     {/*<h1>Hello World!</h1>*/}
   </React.StrictMode>,
 );
