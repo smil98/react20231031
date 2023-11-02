35f3c2ffe4f991a2d27bfee666607dd2ce815485
smil98 <seungmilee98@gmail.com>
2023-11-02 14:57:32 +0900

    FormControl, FormLabel, Input type, Radio, CheckBox

--- src/App.js
+++ src/App.js
@@ -4,36 +4,60 @@ import {
   FormErrorMessage,
   FormHelperText,
   Button,
+  Input,
+  Center,
+  Box,
+  RadioGroup,
+  Radio,
+  Flex,
+  Checkbox,
+  CheckboxGroup,
 } from "@chakra-ui/react";
 import { AddIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
-    <>
-      <Button colorScheme="blue">Lorem.</Button>
-      <Button colorScheme="red">Itaque.</Button>
-      <Button colorScheme="green">Doloremque?</Button>
-      <Button colorScheme="orange">Accusantium.</Button>
-      <Button colorScheme="teal" size={"sm"}>
-        Exercitationem.
-      </Button>
-      <Button colorScheme="cyan" size={"lg"}>
-        Beatae?
-      </Button>
-      <Button colorScheme="telegram" variant="outline">
-        Eos.
-      </Button>
-      <Button colorScheme="orange" vairant="ghost">
-        Beatae?
-      </Button>
-      <Button variant="link">In?</Button>
-      <Button leftIcon={<EmailIcon />}>Alias?</Button>
-      <Button rightIcon={<AddIcon />}>Ut!</Button>
-      <Button isLoading>Fugit.</Button>
-      <Button isLoading loadingText="sending">
-        Placeat?
-      </Button>
-    </>
+    <div>
+      <Center p={5}>
+        <Box w={"480px"}>
+          <FormControl border={"1px solid gray"} borderRadius={5} p={5}>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+            <FormHelperText>Write your Email</FormHelperText>
+          </FormControl>
+        </Box>
+        <br />
+      </Center>
+      <Center>
+        <Box w={"480px"} border={"1px solid blue"}>
+          <FormControl p={5}>
+            <FormLabel>Select One Option Only</FormLabel>
+            <RadioGroup>
+              <Flex gap={5}>
+                <Radio value="value1">Lorem.</Radio>
+                <Radio value="value2">Maiores.</Radio>
+                <Radio value="value3">Corporis?</Radio>
+                <Radio value="value4">Enim.</Radio>
+              </Flex>
+            </RadioGroup>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center>
+        <Box w={"480px"} border={"1px dotted black"}>
+          <FormControl p={5}>
+            <FormLabel>Select Options</FormLabel>
+            <CheckboxGroup>
+              <Flex gap={5}>
+                <Checkbox>Lorem.</Checkbox>
+                <Checkbox>Eaque!</Checkbox>
+                <Checkbox>Sapiente?</Checkbox>
+              </Flex>
+            </CheckboxGroup>
+          </FormControl>
+        </Box>
+      </Center>
+    </div>
   );
 }
 
