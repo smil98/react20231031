a415efdfdbf410636dae7a96cea594586df89d8b
smil98 <seungmilee98@gmail.com>
2023-11-02 14:34:19 +0900

    Button

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import { Button, useToast } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Button,
+} from "@chakra-ui/react";
+import { AddIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "Note",
-            description: "By pressing the buttons, you will create an account",
-            duration: 9000,
-            isClosable: true,
-          })
-        }
-      >
-        What is This?
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Itaque.</Button>
+      <Button colorScheme="green">Doloremque?</Button>
+      <Button colorScheme="orange">Accusantium.</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Exercitationem.
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "Account Created",
-            description: "Account has been sucessfully created",
-            status: "success",
-            duration: 9000,
-            isClosable: true,
-          })
-        }
-      >
-        Create Account(sucess)
+      <Button colorScheme="cyan" size={"lg"}>
+        Beatae?
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "Account Created",
-            description: "Failed to Create an Account",
-            status: "error",
-            duration: 9000,
-            isClosable: true,
-          })
-        }
-      >
-        Create Account(fail)
+      <Button colorScheme="telegram" variant="outline">
+        Eos.
+      </Button>
+      <Button colorScheme="orange" vairant="ghost">
+        Beatae?
+      </Button>
+      <Button variant="link">In?</Button>
+      <Button leftIcon={<EmailIcon />}>Alias?</Button>
+      <Button rightIcon={<AddIcon />}>Ut!</Button>
+      <Button isLoading>Fugit.</Button>
+      <Button isLoading loadingText="sending">
+        Placeat?
       </Button>
     </>
   );
