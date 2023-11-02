24521e0a17bee1a136393bb7f1408ba958512cff
smil98 <seungmilee98@gmail.com>
2023-11-02 17:41:25 +0900

    NavBar Practice

--- src/App.js
+++ src/App.js
@@ -1,43 +1,96 @@
-import { Box, Circle, Image, Stack } from "@chakra-ui/react";
-import { AddIcon, EmailIcon } from "@chakra-ui/icons";
+import React from "react";
+import {
+  Box,
+  Text,
+  Flex,
+  Button,
+  Input,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Show,
+  Hide,
+  Square,
+} from "@chakra-ui/react";
+import { ChevronDownIcon, HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
 
-function App() {
+function App(props) {
   return (
-    <div>
-      <Box boxSize="sm">
-        <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
-      </Box>
-      <Stack direction="column">
-        <Image
-          boxSize="100px"
-          objectFit="cover"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Image
-          boxSize="150px"
-          objectFit="cover"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Image
-          boxSize="200px"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Image
-          borderRadius="full"
-          boxSize="250px"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Image
-          boxSize="300px"
-          src="abcde.jpg"
-          fallbackSrc="https://via.placeholder.com/150"
-        />
-      </Stack>
-    </div>
+    <Flex
+      m={5}
+      p={5}
+      h={100}
+      border={"1px solid black"}
+      justify="space-between"
+      overflowX="hidden"
+    >
+      <Flex align="center">
+        <Flex>
+          <Text fontSize="1.3rem" color="black" fontWeight={"400"} mr={5}>
+            NavBar
+          </Text>
+        </Flex>
+        <Flex gap={2}>
+          <Hide below="md">
+            <Button bg="none" _hover={{ bg: "none" }}>
+              Home
+            </Button>
+            <Button bg="none" _hover={{ bg: "none" }}>
+              Link
+            </Button>
+            <Menu>
+              <MenuButton
+                as={Button}
+                bg="none"
+                _hover={{ bg: "none" }}
+                _expanded={{ bg: "none" }}
+                rightIcon={<ChevronDownIcon />}
+              >
+                Dropdown
+              </MenuButton>
+              <MenuList>
+                <MenuItem>A</MenuItem>
+                <MenuItem>B</MenuItem>
+                <MenuItem borderTop={"1px solid grey"}>C</MenuItem>
+              </MenuList>
+            </Menu>
+            <Button bg="none" _hover={{ bg: "none" }} isDisabled>
+              Disabled
+            </Button>
+          </Hide>
+        </Flex>
+      </Flex>
+      <Flex align="center" gap={2}>
+        <Show below="md">
+          <Menu>
+            <MenuButton as={Button} align="center">
+              <HamburgerIcon fontSize={"1.5rem"} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Home</MenuItem>
+              <MenuItem>Link</MenuItem>
+              <MenuItem>Dropdown</MenuItem>
+              <MenuItem>Disabled</MenuItem>
+              <MenuItem>Search</MenuItem>
+            </MenuList>
+          </Menu>
+        </Show>
+        <Hide below="md">
+          <Input borderRadius={10} placeholder="Search" size={"sm"} />
+          <Button
+            p={5}
+            size={"sm"}
+            variant="outline"
+            leftIcon={<SearchIcon />}
+            colorScheme="green"
+            _hover={{ bg: "green", color: "white", border: "1px solid green" }}
+          >
+            Search
+          </Button>
+        </Hide>
+      </Flex>
+    </Flex>
   );
 }
 
