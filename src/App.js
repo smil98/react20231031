934168bdf6c745fdeca88997c2b18d220cc11274
smil98 <seungmilee98@gmail.com>
2023-11-06 15:15:04 +0900

    If array is used as state, to change it must create a copy of it (practice)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Input, ListItem, UnorderedList } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
+
+  function handleButtonClick() {
+    setItems([...items, text]);
+    setText("");
+  }
+
+  function handleRemoveButtonClick(index) {
+    setItems(items.filter((item, i) => i !== index)); // Use !== to compare for inequality
+  }
+
+  return (
+    <>
+      <Box>
+        <Input value={text} onChange={(e) => setText(e.target.value)} />
+        <Button onClick={handleButtonClick}>ADD</Button>
+      </Box>
+      <Box>
+        <UnorderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>
+              {item}
+              <Button
+                colorScheme="red"
+                onClick={() => handleRemoveButtonClick(index)}
+              >
+                Delete
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </>
+  );
 }
 
 export default App;
