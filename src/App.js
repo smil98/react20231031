07f5614c5d3845b893e779d2d630a4f564ebcdbc
smil98 <seungmilee98@gmail.com>
2023-11-06 12:25:47 +0900

    Use array in jsx, components mapped in listItem must have key prop
    Key prop must be unique, and would normally be db's primary key

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import React, { useState } from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
+import React from "react";
+import { List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
-  // HOOK : function that starts with use
-  // must be written on top of the component
-  // cannot be used inside iteration or conditional functions
+  const arr = ["pizza", "hamburger", "milk", "tea"];
+  const arr2 = ["son", "lee", "kim"];
+  const listItems = arr.map((item, i) => <ListItem key={i}>{item}</ListItem>);
 
-  //set.... : method that changes state
-  //checks whether the state is identical and decides re-rendering
-
-  const [number, setNumber] = useState(0);
-  const [numberObject, setNumberObject] = useState({ number: 0 });
-
-  function handleNumberObjectChange() {
-    // this won't work
-    // numberObject.number = numberObject.number + 1;
-    // setNumberObject(numberObject);
-
-    const newNumberObject = { ...numberObject };
-    newNumberObject.number = newNumberObject.number + 1;
-    setNumberObject(newNumberObject);
-  }
+  // const arr4 = [
+  //   { id: 0, name: "latte" },
+  //   { id: 1, name: "honey" },
+  //   { id: 2, name: "bread" },
+  // ]; would be the ideal way to set index
 
   return (
     <div>
-      <Box>
-        <Button onClick={() => setNumber(number + 1)}>Change Number</Button>
-        <Text>{number}</Text>
-      </Box>
-      <Box>
-        <Button onClick={handleNumberObjectChange}>Change Number Object</Button>
-        <Text>{numberObject.number}</Text>
-      </Box>
+      <List>{listItems}</List>
+      <List>
+        {arr2.map((name, index) => (
+          <ListItem key={index}>{name}</ListItem>
+        ))}
+      </List>
     </div>
   );
 }
