c4e765fbc3b35e802f777ac8d2b07bef527c59c6
smil98 <seungmilee98@gmail.com>
2023-11-06 09:38:49 +0900

    Using State (practice)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [number, setNumber] = useState(0);
+
+  function handleButtonClick() {
+    setNumber(number + 1);
+  }
+
+  function handleButtonClick2() {
+    setNumber(number - 1);
+  }
+
+  return (
+    <>
+      <h1>{number}</h1>
+      <Button onClick={handleButtonClick}>Increase</Button>
+      <Button onClick={handleButtonClick2}>Decrease</Button>
+    </>
+  );
 }
 
 export default App;
