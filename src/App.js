21922ac8f6cf1bdc235748b44fa793cb1820abf6
smil98 <seungmilee98@gmail.com>
2023-11-06 11:28:03 +0900

    Create object's copy and using the copy to change state (practice)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  const [message, setMessage] = useState("");
+  const [obj, setObj] = useState({ message: "" });
+
+  function handleObjectMessageChange(e) {
+    // const newObject = { ...obj };
+    // newObject.message = e.target.value;
+    // setObj(newObject);
+
+    setObj({ ...obj, message: e.target.value });
+  }
+
+  return (
+    <>
+      <Box>
+        <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+        <Text>{message}</Text>
+      </Box>
+      <Box>
+        <Input value={message} onChange={handleObjectMessageChange} />
+        <Text>{obj.message}</Text>
+      </Box>
+    </>
+  );
 }
 
 export default App;
