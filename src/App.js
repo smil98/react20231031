4469b94e1e747329854df311a5455baad10db995
smil98 <seungmilee98@gmail.com>
2023-11-06 11:15:43 +0900

    Create object's copy and using the copy to change state

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import React, { useState } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
+  // HOOK : function that starts with use
+  // must be written on top of the component
+  // cannot be used inside iteration or conditional functions
+
+  //set.... : method that changes state
+  //checks whether the state is identical and decides re-rendering
+
   const [number, setNumber] = useState(0);
+  const [numberObject, setNumberObject] = useState({ number: 0 });
 
-  console.log(number);
+  function handleNumberObjectChange() {
+    // this won't work
+    // numberObject.number = numberObject.number + 1;
+    // setNumberObject(numberObject);
 
-  const index = number % 3;
+    const newNumberObject = { ...numberObject };
+    newNumberObject.number = newNumberObject.number + 1;
+    setNumberObject(newNumberObject);
+  }
 
   return (
     <div>
-      <Button onClick={() => setNumber(number + 1)}>NEXT</Button>
-      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
-        <Text>message1</Text>
+      <Box>
+        <Button onClick={() => setNumber(number + 1)}>Change Number</Button>
+        <Text>{number}</Text>
       </Box>
-      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
-        <Text>message2</Text>
-      </Box>
-      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
-        <Text>message3</Text>
+      <Box>
+        <Button onClick={handleNumberObjectChange}>Change Number Object</Button>
+        <Text>{numberObject.number}</Text>
       </Box>
     </div>
   );
