699278a1d2d8b6883b5e75814e3126703404e9eb
smil98 <seungmilee98@gmail.com>
2023-11-06 14:22:20 +0900

    If array is used as state, to change it must create a copy of it (practice)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, List, ListItem, OrderedList } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick(item) {
+    console.log(item);
+    const newItems = [...items];
+    newItems.push(item);
+    setItems(newItems);
+  }
+
+  return (
+    <>
+      <Button onClick={() => handleButtonClick("coffee")}>Coffee</Button>
+      <Button onClick={() => handleButtonClick("cake")}>Cake</Button>
+      <Button onClick={() => handleButtonClick("iceCream")}>IceCream</Button>
+      <Button onClick={() => handleButtonClick("cookie")}>Cookie</Button>
+      <Button onClick={() => handleButtonClick("apple")}>Apple</Button>
+
+      <Box>
+        <OrderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </OrderedList>
+      </Box>
+    </>
+  );
 }
 
 export default App;
