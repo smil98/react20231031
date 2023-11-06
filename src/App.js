300f531559cd6e7dcb98793c7bd6f344a9c40046
smil98 <seungmilee98@gmail.com>
2023-11-06 16:27:19 +0900

    immer

--- src/App.js
+++ src/App.js
@@ -3,45 +3,36 @@ import { Input, Text } from "@chakra-ui/react";
 import { useImmer } from "use-immer";
 
 function App(props) {
-  const [person, setPerson] = useState({ name: "son", city: "seoul" });
-  const [user, updateUser] = useImmer({ name: "lee", city: "jeju" });
+  const [person, updatePerson] = useImmer({
+    name: "son",
+    address: { city: "seoul", country: "korea" },
+  });
 
   function handleNameChange(e) {
-    const nextPerson = { ...person };
-    nextPerson.name = e.target.value;
-  }
-
-  function handleCityChange(e) {
-    const nextPerson = { ...person };
-    nextPerson.city = e.target.value;
-    setPerson(nextPerson);
-  }
-
-  function handleUserNameChange(e) {
-    updateUser((draft) => {
+    updatePerson((draft) => {
       draft.name = e.target.value;
     });
   }
 
-  function handleUserCityChange(e) {
-    updateUser((draft) => {
-      draft.city = e.target.value;
+  function handleCityChange(e) {
+    updatePerson((draft) => {
+      draft.address.city = e.target.value;
+    });
+  }
+
+  function handleCountryChange(e) {
+    updatePerson((draft) => {
+      draft.address.country = e.target.value;
     });
   }
 
   return (
     <div>
       <Input value={person.name} onChange={handleNameChange} />
-      <Input value={person.city} onChange={handleCityChange} />
+      <Input value={person.address.city} onChange={handleCityChange} />
+      <Input value={person.address.country} onChange={handleCountryChange} />
       <Text>
-        {person.name} lives in {person.city}
-      </Text>
-
-      <hr />
-      <Input value={user.name} onChange={handleUserNameChange} />
-      <Input value={user.city} onChange={handleUserCityChange} />
-      <Text>
-        {user.name} lives in {user.city}
+        {person.name} lives in {person.address.country}, {person.address.city}.
       </Text>
     </div>
   );
