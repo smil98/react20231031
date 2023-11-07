5d5552f2979a8ace19ad703c8e0d77cbc6a32093
smil98 <seungmilee98@gmail.com>
2023-11-07 14:36:09 +0900

    Put state, setState in context value (practice)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import { Box } from "@chakra-ui/react";
+import React, { createContext, useContext, useState } from "react";
+import { Input, Text } from "@chakra-ui/react";
 
-function App() {
-  return <></>;
+function MyInput() {
+  const { setText } = useContext(MessageContext);
+  return (
+    <Input
+      placeholder="Enter Message"
+      onChange={(e) => setText(e.target.value)}
+    />
+  );
 }
+
+function MyText() {
+  const { text } = useContext(MessageContext);
+  return <Text>{text}</Text>;
+}
+
+function App(props) {
+  const [text, setText] = useState("Where Message is Shown");
+  return (
+    <div>
+      <MessageContext.Provider value={{ text: text, setText: setText }}>
+        <MyInput />
+        <MyText />
+      </MessageContext.Provider>
+    </div>
+  );
+}
+
+const MessageContext = createContext(null);
 
 export default App;
