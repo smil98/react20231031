1bbf218800005f5f563a34d5c73236da0b50150c
smil98 <seungmilee98@gmail.com>
2023-11-07 16:26:18 +0900

    ajax get used with state (practice)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App(props) {
+  const [myState, setMyState] = useState("");
+  useEffect(() => {
+    axios
+      .get("/api/main1/sub2")
+      .then((response) => response.data)
+      .then((data) => setMyState(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return <div>{myState}</div>;
+}
+
+export default App;
