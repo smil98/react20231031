d6e2c1630695afe6637e540c2987ac2410074cfe
smil98 <seungmilee98@gmail.com>
2023-11-07 15:19:44 +0900

    deleted unnecessary codes

--- src/component/BComp.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import { Text } from "@chakra-ui/react";
-import { MessageContext } from "../App";
-
-export function BComp() {
-  const value = useContext(MessageContext);
-  return <Text>{value.message}</Text>;
-}
