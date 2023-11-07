8bef7f5478e60e7f7f8e66c03f675054f3964f8d
smil98 <seungmilee98@gmail.com>
2023-11-07 14:39:58 +0900

    Seperating and export/importing components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import React, { createContext, useContext, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
-
-function AComp() {
-  const value = useContext(MessageContext);
-  return (
-    <Button onClick={() => value.setMessage("Changed message")}>Click</Button>
-  );
-}
-
-function BComp() {
-  const value = useContext(MessageContext);
-  return <Text>{value.message}</Text>;
-}
+import React, { createContext, useState } from "react";
+import { AComp } from "./AComp";
+import { BComp } from "./BComp";
 
 function App(props) {
   const [message, setMessage] = useState("Initial Message");
@@ -27,5 +16,5 @@ function App(props) {
   );
 }
 
-const MessageContext = createContext(null);
+export const MessageContext = createContext(null);
 export default App;
