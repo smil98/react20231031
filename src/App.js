b175d490a1dc3c689f9a674f1a76424428221171
smil98 <seungmilee98@gmail.com>
2023-11-08 15:15:23 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -14,6 +14,16 @@ const routes = createBrowserRouter(
       <Route path="/path1" element={<Box>Route1</Box>} />
       <Route path="/path2" element={<Box>Route2</Box>} />
       <Route path="/path3" element={<Box>Route3</Box>} />
+      <Route path="/main1/path1" element={<Box>Route4</Box>} />
+      <Route path="/main1/path2" element={<Box>Route5</Box>} />
+      <Route path="/main2">
+        <Route path="path1" element={<Box>Route6</Box>} />
+        <Route path="path2" element={<Box>Route7</Box>} />
+        <Route path="path3">
+          <Route path="sub1" element={<Box>Route8</Box>} />
+          <Route path="sub2" element={<Box>Route9</Box>} />
+        </Route>
+      </Route>
     </>,
   ),
 );
