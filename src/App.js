180014ba07d92def1e469fc257fd9cad152f5158
smil98 <seungmilee98@gmail.com>
2023-11-08 09:30:03 +0900

    second parameter of useEffect is a value that triggers the first parameter(setup, effect)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
-import axios from "axios";
+import { Button, Input, Text } from "@chakra-ui/react";
 
 function App(props) {
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  //first parameter: function that has side effect
+  //second parameter : array that executes setup
+  //if the array is empty only runs once
   useEffect(() => {
-    axios
-      .get("/api/main1/sub1")
-      .then((response) => response.data)
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  }, []);
-  return <div></div>;
+    console.log("effect function is running");
+  }, [number, text]);
+
+  return (
+    <div>
+      <Button onClick={() => setNumber(number + 1)}>Add</Button>
+      <Text>{number}</Text>
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
+    </div>
+  );
 }
 
 export default App;
